735eaef19547d0dadc25d3080650a8ca675db43e
KarthikCodes <155284320+karthivisva@users.noreply.github.com>
2025-09-04 17:49:22 +0530

    Update Expectations.tsx

--- container/services-page/Expectations.tsx
+++ container/services-page/Expectations.tsx
@@ -62,31 +62,42 @@ export default function Expectations() {
 					className="pb-[50px] lg:pb-[40px] md:pb-[30px] sm:pb-[30px] xm:pb-[15px] text-[540px] leading-[330px] lg:text-[380px] lg:leading-[240px] md:text-[300px] md:leading-[160px] sm:text-[230px] sm:leading-[140px] xm:text-[130px] xm:leading-[80px]"
 				/>
 			</div>
+
 			<div className="w-full padding-x py-[20px]">
 				<div className="w-full flex justify-between sm:flex-col xm:flex-col sm:gap-[20px] xm:gap-[20px]">
+					
+					{/* Left Section */}
 					<div className="w-[50%] sm:w-full xm:w-full">
 						<h3 className="paragraph font-medium text-white font-NeueMontreal">
 							Why Choose APLUSB Consortium?
 						</h3>
 					</div>
+
+					{/* Right Section: Cards */}
 					<div className="w-[50%] sm:w-full xm:w-full flex flex-wrap gap-[20px]">
 						{expectationsItems.map((item) => (
 							<div
-								className="w-[345px] flex justify-between gap-x-[20px] sm:flex-col xm:flex-col gap-[20px]"
-								key={item.id}>
-								<div className="bg-[#145B52] w-full flex flex-col rounded-[20px] px-[30px] py-[20px]">
-									<div className="flex gap-x-[10px] items-center pb-[10px] mb-[100px]">
-										<h1 className="sub-heading font-normal font-NeueMontreal text-white">
+								className="max-w-[345px] flex flex-col sm:w-full xm:w-full"
+								key={item.id}
+							>
+								<div className="bg-[#145B52] w-full flex flex-col rounded-[20px] px-[30px] py-[20px] break-words">
+									
+									{/* Title */}
+									<div className="flex gap-x-[10px] items-center pb-[10px]">
+										<h1 className="sub-heading font-normal font-NeueMontreal text-white break-words">
 											{item.title1}
 										</h1>
 									</div>
+
+									{/* Subtitle + Button */}
 									<div className="w-full flex justify-between items-center">
-										<button className="small-text font-normal font-NeueMontreal text-white">
+										<button className="small-text font-normal font-NeueMontreal text-white text-left break-words">
 											<TextHover titile1={item.subTitle1} titile2={item.subTitle1} />
 										</button>
 										<button
 											onClick={() => handleButtonClick(item.id)}
-											className="small-text uppercase font-normal font-NeueMontreal text-white">
+											className="small-text uppercase font-normal font-NeueMontreal text-white"
+										>
 											{openItemId === item.id ? (
 												"hide"
 											) : (
@@ -94,6 +105,8 @@ export default function Expectations() {
 											)}
 										</button>
 									</div>
+
+									{/* Expanded Content */}
 									<AnimatePresence>
 										{openItemId === item.id && (
 											<motion.div
@@ -103,8 +116,9 @@ export default function Expectations() {
 												transition={{
 													ease: [0.4, 0, 0.2, 1],
 													duration: 1,
-												}}>
-												<div className="border-t border-[#f1f1f155] pt-[20px] text-background mt-[10px]">
+												}}
+											>
+												<div className="border-t border-[#f1f1f155] pt-[20px] text-background mt-[10px] break-words">
 													{item.para1}
 												</div>
 											</motion.div>
