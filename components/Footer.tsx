bff1ffb9bcf65fd283864c9fec479109ceabcde8
KarthikCodes <155284320+karthivisva@users.noreply.github.com>
2025-09-04 17:39:40 +0530

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,126 +5,134 @@ import { LinkHover, TextMask } from "@/animation";
 import { footerItems, footernavbarItems } from "@/constants";
 
 export default function Footer() {
-	const phrase = ["a plus b", "Consortium"];
-	const phrase1 = ["Other Detials"];
-	return (
-		<footer className="w-full min-h-screen padding-x z-30 relative pt-[40px] bg-background flex flex-col justify-between rounded-t-[20px] mt-[-20px]">
-			<div className="w-full flex justify-between sm:flex-col xm:flex-col">
-				<div className="flex flex-col justify-between sm:w-full xm:w-full w-1/2">
-					<h1 className="text-[150px] leading-[115px] lg:text-[130px] lg:leading-[98px] md:text-[100px] md:leading-[75px] sm:text-[74px] sm:leading-[68px] xm:text-[64px] xm:leading-[48px] font-semibold font-FoundersGrotesk text-secondry uppercase">
-						<TextMask>{phrase}</TextMask>
-					</h1>
-				</div>
-				<div className="h-full flex flex-col justify-between sm:w-full xm:w-full w-1/2">
-					<div>
-						<h1 className="text-[150px] leading-[115px] lg:text-[130px] lg:leading-[98px] md:text-[100px] md:leading-[75px] sm:text-[74px] sm:leading-[68px] xm:text-[64px] xm:leading-[48px] font-semibold font-FoundersGrotesk text-secondry uppercase">
-							<TextMask>{phrase1}</TextMask>
-						</h1>
-						<div className="pt-[50px]">
-							<h1 className="paragraph font-medium font-NeueMontreal text-secondry pb-[20px]">
-							Connect with us:
-							</h1>
-							{footerItems.map((item) => (
-								<LinkHover
-									title={item.title}
-									href={item.href}
-									key={item.id}
-									className="before:h-[1px] after:h-[1px] w-fit paragraph font-medium text-secondry capitalize flex flex-col before:bottom-[1px] after:bottom-[1px]"
-								/>
-							))}
-						</div>
-						<div className="flex justify-between">
-							<div className="pt-[50px]">
-								<h1 className="paragraph font-medium font-NeueMontreal text-secondry pb-[20px]">
-								Location:
-								</h1>
-								<div className="flex flex-col gap-y-[10px]">
-									<LinkHover
-										className="before:h-[1px] after:h-[1px] w-fit paragraph font-medium  capitalize flex flex-col before:bottom-[1px] after:bottom-[1px]"
-										title={`202-1965 W 4th Awe`}
-										href="/"
-									/>
-									<LinkHover
-										className="before:h-[1px] after:h-[1px] w-fit paragraph font-medium  capitalize flex flex-col before:bottom-[1px] after:bottom-[1px]"
-										title={`,
-Ramnagar, `}
-										href="/"
-									/>
-									<LinkHover
-										className="before:h-[1px] after:h-[1px] w-fit paragraph font-medium  capitalize flex flex-col before:bottom-[1px] after:bottom-[1px]"
-										title="Coimbatore - 641 009,"
-										href="/"
-									/>
-									<LinkHover
-										className="before:h-[1px] after:h-[1px] w-fit paragraph font-medium  capitalize flex flex-col before:bottom-[1px] after:bottom-[1px]"
-										title="134, 3rd floor,"
-										href="/"
-									/>
-									<LinkHover
-										className="before:h-[1px] after:h-[1px] w-fit paragraph font-medium  capitalize flex flex-col before:bottom-[1px] after:bottom-[1px]"
-										title=" Kalidas road"
-										href="/"
-									/>
-								</div>
-							</div>
-							<div className="pt-[50px]">
-								<h1 className="paragraph font-medium font-NeueMontreal text-secondry pb-[20px]">
-									Quick Navigation:
-								</h1>
-								{footernavbarItems.map((item) => (
-									<LinkHover
-										key={item.id}
-										title={item.title}
-										href={item.href}
-										className="before:h-[1px] after:h-[1px] w-fit paragraph font-medium text-secondry capitalize flex flex-col before:bottom-[1px] after:bottom-[1px]"
-									/>
-								))}
-							</div>
-						</div>
-						<div className="pt-[50px] flex gap-x-[20px]">
-							<h1 className="paragraph font-medium font-NeueMontreal text-secondry">
-							Mail:
-							</h1>
-							<LinkHover
-								title="aplusbconsortium@gmail.com"
-								href="/"
-								className="before:h-[1px] after:h-[1px] paragraph font-medium before:bottom-[-3px] after:bottom-[-3px]"
-							/>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div className="w-full pt-[40px] pb-[30px] flex justify-between sm:flex-col xm:flex-col sm:gap-[20px] xm:gap-[20px]">
-				<div className="w-1/2 sm:w-full xm:w-full">
-					<Link href={"/"}>
-						<Image
-							src={logo}
-							alt="ochi logo"
-							width={70}
-							height={70}
-						/>
-					</Link>
-				</div>
-				<div className="w-1/2 h-full flex gap-[10px] justify-between items-end sm:w-full xm:w-full sm:flex-col xm:flex-col sm:items-start xm:items-start">
-					<div className="flex sm:flex-col xm:flex-col gap-[10px]">
-						<h1 className="paragraph font-medium font-NeueMontreal text-secondry opacity-40">
-							© a plus b consortium design 2025.
-						</h1>
-						<LinkHover
-							title="Legal Terms"
-							href="/"
-							className="before:h-[1px] after:h-[1px] paragraph font-medium text-secondry opacity-40 before:bottom-[-3px] after:bottom-[-3px]"
-						/>
-					</div>
-					<div>
-						<LinkHover
-							title="Website by Quantaweave"
-							href="www.quantaweave.in"
-							className="before:h-[1px] after:h-[1px] paragraph font-medium text-secondry opacity-40 before:bottom-[-3px] after:bottom-[-3px]"
-						/>
-					</div>
-				</div>
-			</div>
-		</footer>
-	);
+  const phrase = ["a plus b", "Consortium"];
+  const phrase1 = ["Other Details"];
+
+  return (
+    <footer className="w-full min-h-screen padding-x z-30 relative pt-[40px] bg-background flex flex-col justify-between rounded-t-[20px] mt-[-20px]">
+      <div className="w-full flex justify-between sm:flex-col xm:flex-col">
+        <div className="flex flex-col justify-between sm:w-full xm:w-full w-1/2">
+          <h1 className="text-[150px] leading-[115px] lg:text-[130px] lg:leading-[98px] md:text-[100px] md:leading-[75px] sm:text-[74px] sm:leading-[68px] xm:text-[64px] xm:leading-[48px] font-semibold font-FoundersGrotesk text-secondry uppercase">
+            <TextMask>{phrase}</TextMask>
+          </h1>
+        </div>
+
+        <div className="h-full flex flex-col justify-between sm:w-full xm:w-full w-1/2">
+          <div>
+            <h1 className="text-[150px] leading-[115px] lg:text-[130px] lg:leading-[98px] md:text-[100px] md:leading-[75px] sm:text-[74px] sm:leading-[68px] xm:text-[64px] xm:leading-[48px] font-semibold font-FoundersGrotesk text-secondry uppercase">
+              <TextMask>{phrase1}</TextMask>
+            </h1>
+
+            {/* 🔹 Connect with us */}
+            <div className="pt-[50px]">
+              <h1 className="paragraph font-medium font-NeueMontreal text-secondry pb-[20px]">
+                Connect with us:
+              </h1>
+              {footerItems.map((item) => (
+                <LinkHover
+                  title={item.title}
+                  href={item.href}
+                  key={item.id}
+                  className="before:h-[1px] after:h-[1px] w-fit paragraph font-medium text-secondry capitalize flex flex-col before:bottom-[1px] after:bottom-[1px]"
+                />
+              ))}
+            </div>
+
+            <div className="flex justify-between">
+              {/* 🔹 Location */}
+              <div className="pt-[50px]">
+                <h1 className="paragraph font-medium font-NeueMontreal text-secondry pb-[20px]">
+                  Location:
+                </h1>
+                <div className="flex flex-col gap-y-[10px]">
+                  <LinkHover
+                    className="before:h-[1px] after:h-[1px] w-fit paragraph font-medium capitalize flex flex-col before:bottom-[1px] after:bottom-[1px]"
+                    title="134, 3rd floor,"
+                    href="/"
+                  />
+                  <LinkHover
+                    className="before:h-[1px] after:h-[1px] w-fit paragraph font-medium capitalize flex flex-col before:bottom-[1px] after:bottom-[1px]"
+                    title="Kalidas road,"
+                    href="/"
+                  />
+                  <LinkHover
+                    className="before:h-[1px] after:h-[1px] w-fit paragraph font-medium capitalize flex flex-col before:bottom-[1px] after:bottom-[1px]"
+                    title="Ramnagar, Coimbatore - 641 009"
+                    href="/"
+                  />
+                </div>
+              </div>
+
+              {/* 🔹 Quick Navigation */}
+              <div className="pt-[50px]">
+                <h1 className="paragraph font-medium font-NeueMontreal text-secondry pb-[20px]">
+                  Quick Navigation:
+                </h1>
+                {footernavbarItems.map((item) => (
+                  <LinkHover
+                    key={item.id}
+                    title={item.title}
+                    href={item.href}
+                    className="before:h-[1px] after:h-[1px] w-fit paragraph font-medium text-secondry capitalize flex flex-col before:bottom-[1px] after:bottom-[1px]"
+                  />
+                ))}
+              </div>
+            </div>
+
+            {/* 🔹 Mail + Website */}
+            <div className="pt-[50px] flex flex-col gap-y-[10px]">
+              <div className="flex gap-x-[20px]">
+                <h1 className="paragraph font-medium font-NeueMontreal text-secondry">
+                  Mail:
+                </h1>
+                <LinkHover
+                  title="aplusbconsortium@gmail.com"
+                  href="mailto:aplusbconsortium@gmail.com"
+                  className="before:h-[1px] after:h-[1px] paragraph font-medium before:bottom-[-3px] after:bottom-[-3px]"
+                />
+              </div>
+              <div className="flex gap-x-[20px]">
+                <h1 className="paragraph font-medium font-NeueMontreal text-secondry">
+                  Website:
+                </h1>
+                <LinkHover
+                  title="aplusbconsortium.com"
+                  href="https://aplusbconsortium.com"
+                  className="before:h-[1px] after:h-[1px] paragraph font-medium before:bottom-[-3px] after:bottom-[-3px]"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* 🔹 Footer Bottom */}
+      <div className="w-full pt-[40px] pb-[30px] flex justify-between sm:flex-col xm:flex-col sm:gap-[20px] xm:gap-[20px]">
+        <div className="w-1/2 sm:w-full xm:w-full">
+          <Link href={"/"}>
+            <Image src={logo} alt="ochi logo" width={70} height={70} />
+          </Link>
+        </div>
+        <div className="w-1/2 h-full flex gap-[10px] justify-between items-end sm:w-full xm:w-full sm:flex-col xm:flex-col sm:items-start xm:items-start">
+          <div className="flex sm:flex-col xm:flex-col gap-[10px]">
+            <h1 className="paragraph font-medium font-NeueMontreal text-secondry opacity-40">
+              © a plus b consortium design 2025.
+            </h1>
+            <LinkHover
+              title="Legal Terms"
+              href="/"
+              className="before:h-[1px] after:h-[1px] paragraph font-medium text-secondry opacity-40 before:bottom-[-3px] after:bottom-[-3px]"
+            />
+          </div>
+          <div>
+            <LinkHover
+              title="Website by Quantaweave"
+              href="https://www.quantaweave.in"
+              className="before:h-[1px] after:h-[1px] paragraph font-medium text-secondry opacity-40 before:bottom-[-3px] after:bottom-[-3px]"
+            />
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
