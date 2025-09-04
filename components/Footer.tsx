f5e69221c37b2876f9fd252ef81e052487863e20
KarthikCodes <155284320+karthivisva@users.noreply.github.com>
2025-09-04 17:43:11 +0530

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -10,13 +10,15 @@ export default function Footer() {
 
   return (
     <footer className="w-full min-h-screen padding-x z-30 relative pt-[40px] bg-background flex flex-col justify-between rounded-t-[20px] mt-[-20px]">
-      <div className="w-full flex justify-between sm:flex-col xm:flex-col">
+      <div className="w-full flex justify-between sm:flex-col xm:flex-col gap-[40px]">
+        {/* Left Side */}
         <div className="flex flex-col justify-between sm:w-full xm:w-full w-1/2">
           <h1 className="text-[150px] leading-[115px] lg:text-[130px] lg:leading-[98px] md:text-[100px] md:leading-[75px] sm:text-[74px] sm:leading-[68px] xm:text-[64px] xm:leading-[48px] font-semibold font-FoundersGrotesk text-secondry uppercase">
             <TextMask>{phrase}</TextMask>
           </h1>
         </div>
 
+        {/* Right Side */}
         <div className="h-full flex flex-col justify-between sm:w-full xm:w-full w-1/2">
           <div>
             <h1 className="text-[150px] leading-[115px] lg:text-[130px] lg:leading-[98px] md:text-[100px] md:leading-[75px] sm:text-[74px] sm:leading-[68px] xm:text-[64px] xm:leading-[48px] font-semibold font-FoundersGrotesk text-secondry uppercase">
@@ -24,82 +26,78 @@ export default function Footer() {
             </h1>
 
             {/* 🔹 Connect with us */}
-            <div className="pt-[50px]">
-              <h1 className="paragraph font-medium font-NeueMontreal text-secondry pb-[20px]">
+            <div className="pt-[40px]">
+              <h1 className="paragraph font-medium font-NeueMontreal text-secondry pb-[15px]">
                 Connect with us:
               </h1>
-              {footerItems.map((item) => (
-                <LinkHover
-                  title={item.title}
-                  href={item.href}
-                  key={item.id}
-                  className="before:h-[1px] after:h-[1px] w-fit paragraph font-medium text-secondry capitalize flex flex-col before:bottom-[1px] after:bottom-[1px]"
-                />
-              ))}
-            </div>
-
-            <div className="flex justify-between">
-              {/* 🔹 Location */}
-              <div className="pt-[50px]">
-                <h1 className="paragraph font-medium font-NeueMontreal text-secondry pb-[20px]">
-                  Location:
-                </h1>
-                <div className="flex flex-col gap-y-[10px]">
+              <div className="flex flex-col gap-y-[8px]">
+                {footerItems.map((item) => (
                   <LinkHover
-                    className="before:h-[1px] after:h-[1px] w-fit paragraph font-medium capitalize flex flex-col before:bottom-[1px] after:bottom-[1px]"
-                    title="134, 3rd floor,"
-                    href="/"
-                  />
-                  <LinkHover
-                    className="before:h-[1px] after:h-[1px] w-fit paragraph font-medium capitalize flex flex-col before:bottom-[1px] after:bottom-[1px]"
-                    title="Kalidas road,"
-                    href="/"
-                  />
-                  <LinkHover
-                    className="before:h-[1px] after:h-[1px] w-fit paragraph font-medium capitalize flex flex-col before:bottom-[1px] after:bottom-[1px]"
-                    title="Ramnagar, Coimbatore - 641 009"
-                    href="/"
-                  />
-                </div>
-              </div>
-
-              {/* 🔹 Quick Navigation */}
-              <div className="pt-[50px]">
-                <h1 className="paragraph font-medium font-NeueMontreal text-secondry pb-[20px]">
-                  Quick Navigation:
-                </h1>
-                {footernavbarItems.map((item) => (
-                  <LinkHover
-                    key={item.id}
                     title={item.title}
                     href={item.href}
-                    className="before:h-[1px] after:h-[1px] w-fit paragraph font-medium text-secondry capitalize flex flex-col before:bottom-[1px] after:bottom-[1px]"
+                    key={item.id}
+                    className="w-fit paragraph font-medium text-secondry capitalize"
                   />
                 ))}
               </div>
             </div>
 
+            <div className="flex justify-between sm:flex-col xm:flex-col sm:gap-[30px] xm:gap-[30px]">
+              {/* 🔹 Location */}
+              <div className="pt-[40px]">
+                <h1 className="paragraph font-medium font-NeueMontreal text-secondry pb-[15px]">
+                  Location:
+                </h1>
+                <div className="flex flex-col gap-y-[6px] paragraph font-medium text-secondry">
+                  <p>134, 3rd Floor,</p>
+                  <p>Kalidas Road,</p>
+                  <p>Ramnagar, Coimbatore - 641 009</p>
+                </div>
+              </div>
+
+              {/* 🔹 Quick Navigation */}
+              <div className="pt-[40px]">
+                <h1 className="paragraph font-medium font-NeueMontreal text-secondry pb-[15px]">
+                  Quick Navigation:
+                </h1>
+                <div className="flex flex-col gap-y-[8px]">
+                  {footernavbarItems.map((item) => (
+                    <LinkHover
+                      key={item.id}
+                      title={item.title}
+                      href={item.href}
+                      className="w-fit paragraph font-medium text-secondry capitalize"
+                    />
+                  ))}
+                </div>
+              </div>
+            </div>
+
             {/* 🔹 Mail + Website */}
-            <div className="pt-[50px] flex flex-col gap-y-[10px]">
-              <div className="flex gap-x-[20px]">
+            <div className="pt-[40px] flex flex-col gap-y-[12px]">
+              <div className="flex gap-x-[10px] sm:flex-col sm:gap-y-[4px]">
                 <h1 className="paragraph font-medium font-NeueMontreal text-secondry">
                   Mail:
                 </h1>
-                <LinkHover
-                  title="aplusbconsortium@gmail.com"
+                <a
                   href="mailto:aplusbconsortium@gmail.com"
-                  className="before:h-[1px] after:h-[1px] paragraph font-medium before:bottom-[-3px] after:bottom-[-3px]"
-                />
+                  className="paragraph font-medium text-secondry"
+                >
+                  aplusbconsortium@gmail.com
+                </a>
               </div>
-              <div className="flex gap-x-[20px]">
+              <div className="flex gap-x-[10px] sm:flex-col sm:gap-y-[4px]">
                 <h1 className="paragraph font-medium font-NeueMontreal text-secondry">
                   Website:
                 </h1>
-                <LinkHover
-                  title="aplusbconsortium.com"
+                <a
                   href="https://aplusbconsortium.com"
-                  className="before:h-[1px] after:h-[1px] paragraph font-medium before:bottom-[-3px] after:bottom-[-3px]"
-                />
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="paragraph font-medium text-secondry"
+                >
+                  aplusbconsortium.com
+                </a>
               </div>
             </div>
           </div>
@@ -121,14 +119,14 @@ export default function Footer() {
             <LinkHover
               title="Legal Terms"
               href="/"
-              className="before:h-[1px] after:h-[1px] paragraph font-medium text-secondry opacity-40 before:bottom-[-3px] after:bottom-[-3px]"
+              className="paragraph font-medium text-secondry opacity-40"
             />
           </div>
           <div>
             <LinkHover
               title="Website by Quantaweave"
               href="https://www.quantaweave.in"
-              className="before:h-[1px] after:h-[1px] paragraph font-medium text-secondry opacity-40 before:bottom-[-3px] after:bottom-[-3px]"
+              className="paragraph font-medium text-secondry opacity-40"
             />
           </div>
         </div>
